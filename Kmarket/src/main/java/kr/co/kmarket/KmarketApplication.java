6e2d4ce27b5651443f4ad9ed0a8669557ec94817
hochmun <s01099760587@gmail.com>
2023-02-07 15:47:50 +0900

    2023/02/07 심규영 프로젝트 추가

---
+++ Kmarket/src/main/java/kr/co/kmarket/KmarketApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmarketApplication.class, args);
+	}
+
+}
---
+++ Kmarket/src/test/java/kr/co/kmarket/KmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
