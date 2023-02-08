d523bd65c2dce83bfe767144539daaa3622fd0fc
aowlrxm <aowlrxm15>
2023-02-08 19:03:00 +0900

    2023-02-08 전인준 멤버 vo,controller

--- Kmarket/src/main/java/kr/co/kmarket/controller/MemberController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/MemberController.java
@@ -1,4 +1,35 @@
 package kr.co.kmarket.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class MemberController {
+
+    @GetMapping("member/join")
+    public String join(){
+        return "member/join";
+    }
+    @GetMapping("member/login")
+    public String login(){
+        return "member/login";
+    }
+    @GetMapping("member/register")
+    public String register(){
+        return "member/register";
+    }
+    @GetMapping("member/registerSeller")
+    public String registerSeller(){
+        return "member/registerSeller";
+    }
+    @GetMapping("member/signup")
+    public String signup(){
+        return "member/signup";
+    }
+ @GetMapping("member/signupSeller")
+    public String signupSeller(){
+        return "member/signupSeller";
+    }
+
 }
--- Kmarket/src/main/java/kr/co/kmarket/vo/memberVO.java
+++ Kmarket/src/main/java/kr/co/kmarket/vo/memberVO.java
@@ -1,4 +1,46 @@
 package kr.co.kmarket.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class memberVO {
+    private String uid;
+    private String pass;
+    private String name;
+    private int gender;
+    private String hp;
+    private String email;
+    private int type;
+    private int point;
+    private int level;
+    private String zip;
+    private String addr1;
+    private String addr2;
+    private String company;
+    private String ceo;
+    private String bizRegNum;
+    private String cornRegNum;
+    private String tel;
+    private String manager;
+    private String managerHp;
+    private String fax;
+    private String regip;
+    private String wdate;
+    private String rdate;
+    private int etc1;
+    private int etc2;
+    private String etc3;
+    private String etc4;
+    private String etc5;
+
+    // (추가) Gender : int -> String
+    public void setGender(String gender) {
+        this.gender = Integer.parseInt(gender);
+    }
 }
