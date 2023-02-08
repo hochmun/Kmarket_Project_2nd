34cb518cd471dee2332fb842fbb7837b1d5b86be
hochmun <s01099760587@gmail.com>
2023-02-08 19:34:21 +0900

    2023/02/08 심규영 add

--- Kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/MainController.java
@@ -1,4 +1,14 @@
 package kr.co.kmarket.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping(value = {"/", "/index"})
+    public String index() {
+        return "index";
+    }
+
 }
