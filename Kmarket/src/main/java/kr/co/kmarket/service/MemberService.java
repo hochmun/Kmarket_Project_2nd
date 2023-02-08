31fbc474cc6deb9b2e0ec9593dd85f68b1a61d0b
aowlrxm <aowlrxm15>
2023-02-08 19:11:30 +0900

    2023-02-08 전인준 멤버 vo,controller,service

--- Kmarket/src/main/java/kr/co/kmarket/service/MemberService.java
+++ Kmarket/src/main/java/kr/co/kmarket/service/MemberService.java
@@ -1,4 +1,13 @@
 package kr.co.kmarket.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
+    public void insertMember(){}
+    public void selectMember(){}
+    public void selectMembers(){}
+    public void updateMember(){}
+    public void deleteMember(){}
+
 }
