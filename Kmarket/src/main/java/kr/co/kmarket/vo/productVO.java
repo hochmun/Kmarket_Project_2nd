19805cf6800acf7e38d8ce768d9d1aa062f5cccc
094haley <094haley@gmail.com>
2023-02-08 16:46:35 +0900

    2023/02/08 이해빈 상품 complete html 수정

--- Kmarket/src/main/java/kr/co/kmarket/vo/productVO.java
+++ Kmarket/src/main/java/kr/co/kmarket/vo/productVO.java
@@ -1,4 +1,50 @@
 package kr.co.kmarket.vo;
+/*
+*  날짜 : 2023/02/08
+*  이름 : 이해빈
+*  내용 : 상품 VO
+*
+* */
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class productVO {
+
+    private int prodNo;
+    private int prodCate1;
+    private int prodCate2;
+    private String prodName;
+    private String descript;
+    private String company;
+    private String seller;
+    private int price;
+    private int discount;
+    private int point;
+    private int stock;
+    private int sold;
+    private int delivery;
+    private int hit;
+    private int score;
+    private int review;
+    private String thumb1;
+    private String thumb2;
+    private String thumb3;
+    private String detail;
+    private String status;
+    private String duty;
+    private String receipt;
+    private String bizType;
+    private String origin;
+    private String ip;
+    private String rdate;
+    private int etc1;
+    private int etc2;
+    private String etc3;
+    private String uid;
+    private String etc5;
 }
