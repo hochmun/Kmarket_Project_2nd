ea6d0af90e42601991c37db9032928e738e6125d
realKimJaeJUN <nagandr1113@gmail.com>
2023-02-16 19:49:43 +0900

    23/02/16 김재준 notice/list 페이징 완료

--- Kmarket/src/main/java/kr/co/kmarket/controller/cs/notice/NoticeViewController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/cs/notice/NoticeViewController.java
@@ -17,11 +17,11 @@ public class NoticeViewController {
     public String view(Model model, Integer noticeNo, String noCate1){
         // noCate 값이 없을시 '전체'로 이동
         if (noCate1 == null || noCate1.equals("")) {
-            noCate1 = "";
+            noCate1 = "%%";
         }
 
         // noCate 값을 정해진 값 이외의 값으로 입력시 에러페이지로 이동
-        if (!noCate1.equals("") && !noCate1.equals("10") && !noCate1.equals("11") && !noCate1.equals("12") && !noCate1.equals("13")) {
+        if (!noCate1.equals("%%") && !noCate1.equals("10") && !noCate1.equals("11") && !noCate1.equals("12") && !noCate1.equals("13")) {
             return "alert";
         }
 
