99e2c1c7258fb1146b278698c0820f0ee6b68907
hochmun <s01099760587@gmail.com>
2023-02-20 18:34:04 +0900

    2023/02/20 // 심규영 // 마이페이지, 서비스 이용 약관 기본 맵핑 작업

--- Kmarket/src/main/java/kr/co/kmarket/controller/TermsPolicyController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/TermsPolicyController.java
@@ -10,4 +10,49 @@ public class TermsPolicyController {
     public String termsPolicy(){
         return "/termsPolicy/policy";
     }
+
+    /**
+     * 2023/02/20 // 심규영 // 기본 맵핑 생성
+     * @return
+     */
+    @GetMapping("/termsPolicy/buyer")
+    public String buyer(){
+        return "/termsPolicy/buyer";
+    }
+
+    /**
+     * 2023/02/20 // 심규영 // 기본 맵핑 생성
+     * @return
+     */
+    @GetMapping("/termsPolicy/seller")
+    public String seller(){
+        return "/termsPolicy/seller";
+    }
+
+    /**
+     * 2023/02/20 // 심규영 // 기본 맵핑 생성
+     * @return
+     */
+    @GetMapping("/termsPolicy/finance")
+    public String finance(){
+        return "/termsPolicy/finance";
+    }
+
+    /**
+     * 2023/02/20 // 심규영 // 기본 맵핑 생성
+     * @return
+     */
+    @GetMapping("/termsPolicy/location")
+    public String location(){
+        return "/termsPolicy/location";
+    }
+
+    /**
+     * 2023/02/20 // 심규영 // 기본 맵핑 생성
+     * @return
+     */
+    @GetMapping("/termsPolicy/privacy")
+    public String privacy(){
+        return "/termsPolicy/privacy";
+    }
 }
