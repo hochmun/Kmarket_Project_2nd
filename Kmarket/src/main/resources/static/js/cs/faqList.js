802bdd1c997262db06fa9011fcd7af6f74b5ee49
hochmun <s01099760587@gmail.com>
2023-02-21 15:12:34 +0900

    2023/02/21 // 심규영 // 자주묻는 질문 리스트

--- Kmarket/src/main/resources/static/js/cs/faqList.js
+++ Kmarket/src/main/resources/static/js/cs/faqList.js
@@ -40,6 +40,33 @@ $(function() {
   });
 });
 
+window.addEventListener('load',()=>{
+
+   console.log('1');
+
+  const faqLists = document.querySelectorAll('.faq-list');
+
+
+  faqLists.forEach((faqList)=>{
+
+        console.log(faqList.childElementCount);
+
+      if (faqList.childElementCount == 1) {
+
+        console.log('2');
+        const newLi = document.createElement('li');
+        const newLink = document.createElement('a');
+
+        newLink.textContent = '게시물이 없습니다.';
+
+        newLi.appendChild(newLink);
+
+        faqList.prepend(newLi);
+
+      }
+  });
+  
+})
 
 
 
