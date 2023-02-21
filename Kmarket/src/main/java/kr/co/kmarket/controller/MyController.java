67d074967fe5d2a9a3ca96ccfaf9aa2e08a3e221
hochmun <s01099760587@gmail.com>
2023-02-21 19:21:53 +0900

    2023/02/21 // 심규영 // 최근 주문 내역 5개 불러오기 기능 완료 포인트 적립 내역 5개 불러오기 기능 완료 문의내역 5개 불러오기 기능 완료
    상품평은 상품쓰기 기능 구현 후 구현 예정
    
    팝업창 닫기 구현

--- Kmarket/src/main/java/kr/co/kmarket/controller/MyController.java
+++ Kmarket/src/main/java/kr/co/kmarket/controller/MyController.java
@@ -1,10 +1,19 @@
 package kr.co.kmarket.controller;
 
+import kr.co.kmarket.entity.UserEntity;
+import kr.co.kmarket.security.MyUserDetails;
 import kr.co.kmarket.service.MyService;
+import kr.co.kmarket.vo.Cs_QnaVO;
+import kr.co.kmarket.vo.member_pointVO;
+import kr.co.kmarket.vo.product_orderVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * 2023/02/20 // 심규영 // 기본 맵핑 생성
@@ -23,7 +32,28 @@ public class MyController {
      * @return
      */
     @GetMapping("my/home")
-    public String home() {
+    public String home(Model model,
+                       @AuthenticationPrincipal MyUserDetails myUserDetails) {
+        // 유저 정보 불러오기
+        UserEntity user = myUserDetails.getUser();
+        
+        // 최근 주문 내역
+        List<product_orderVO> orderVOs = service.selectMyOrder(user.getUid());
+
+        // 포인트 적립 내역
+        List<member_pointVO> pointVOs = service.selectMyPointList5(user.getUid());
+
+        // 상품평 내역
+
+        // 문의 내역 내림 차순
+        List<Cs_QnaVO> qnaVOs = service.selectMyQnaList5(user.getUid());
+
+        // 전송 모델
+        model.addAttribute("orderVOs", orderVOs);
+        model.addAttribute("pointVOs", pointVOs);
+        model.addAttribute("qnaVOs", qnaVOs);
+
+        // 리턴
         return "my/home";
     }
 
--- Kmarket/src/main/java/kr/co/kmarket/vo/member_pointVO.java
+++ Kmarket/src/main/java/kr/co/kmarket/vo/member_pointVO.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 @Data
@@ -17,4 +19,26 @@ public class member_pointVO {
     private int ordNo;
     private int point;
     private Date pointDate;
+    
+    // 출력 포맷
+
+    /**
+     * 2023/02/21 // 심규영 // 적립 날짜 출력
+     * @return
+     */
+    public String getPointDateString() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return simpleDateFormat.format(pointDate);
+    }
+
+    public String getLastDateString() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        cal.setTime(pointDate);
+        cal.add(Calendar.YEAR, 2);
+
+        return simpleDateFormat.format(cal.getTime());
+    }
+
 }
